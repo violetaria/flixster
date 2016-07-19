2d919de35ecc38ffd6942987d2e7a5c6493fcea9
violetaria <terri.chu@outlook.com>
2016-07-18 22:21:48 -0400

    initial setup of http sync

--- app/src/main/java/com/getlosthere/flickster/MovieActivity.java
+++ app/src/main/java/com/getlosthere/flickster/MovieActivity.java
@@ -2,6 +2,16 @@ package com.getlosthere.flickster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -9,5 +19,30 @@ public class MovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url,new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray movieJSONResults = null;
+
+                try {
+                    movieJSONResults = response.getJSONArray("results");
+                    Log.d("DEBUG",movieJSONResults.toString());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/getlosthere/flickster/models/Movie.java
@@ -0,0 +1,7 @@
+package com.getlosthere.flickster.models;
+
+/**
+ * Created by violetaria on 7/18/16.
+ */
+public class Movie {
+}
